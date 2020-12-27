287417d9da4a41c93d0b1f911d4d3dc39892b6ad
Vladimir Petrenko <vladipetrenko@googlemail.com>
2020-12-27 01:00:49 +0100

    V 0.1.2 Implemented preview for dialog ui

--- addons/dialogue_editor/scenes/scenes/DialogueScenePreview.gd
+++ addons/dialogue_editor/scenes/scenes/DialogueScenePreview.gd
@@ -1,21 +1,28 @@
-extends ViewportContainer
+extends ScrollContainer
 
-var zoom = 1.0
-var size = Vector2(800, 600)
+var _scale_factor = 1
+var _loaded_scene
+var _drag = false
 
-func _ready() -> void:
-	$Viewport.set_size(size)
-	var DialogueActorDialog = load("res://addons/dialogue_editor/default/DialogueActorLeft.tscn")
-	var scene = DialogueActorDialog.instance()
-	$Viewport.add_child(scene)
+func _ready():
+	var LoadedScene = load("res://addons/dialogue_editor/default/DialogueActorLeft.tscn")
+	_loaded_scene = LoadedScene.instance() as CanvasLayer
+	add_child(_loaded_scene)
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if (event.button_index == BUTTON_WHEEL_UP):
-			print("wheel up (event)")
-			zoom += 0.1
-			$Viewport.set_size_override(true, size * zoom)
-		if (event.button_index == BUTTON_WHEEL_DOWN):
-			print("wheel down (event)")
-			zoom -= 0.1
-			$Viewport.set_size_override(true, size * zoom)
+			_scale_factor = 1.01
+		elif (event.button_index == BUTTON_WHEEL_DOWN):
+			_scale_factor = 0.99
+		elif event.button_index == BUTTON_MIDDLE:
+			if event.pressed:
+				_drag = true
+			else:
+				_drag = false
+		var transform = _loaded_scene.transform.scaled(Vector2(_scale_factor, _scale_factor))
+		_loaded_scene.set_transform(transform)
+	
+	if event is InputEventMouseMotion and _drag:
+		var transform = _loaded_scene.transform.translated(event.relative)
+		_loaded_scene.set_transform(transform)
