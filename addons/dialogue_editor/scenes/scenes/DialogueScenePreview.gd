99cfb24fe3fed8af123161e7560413abbaed8532
Vladimir Petrenko <vladipetrenko@gmx.de>
2021-01-01 22:51:56 +0100

    V 0.2.3 Preview scale done

--- addons/dialogue_editor/scenes/scenes/DialogueScenePreview.gd
+++ addons/dialogue_editor/scenes/scenes/DialogueScenePreview.gd
@@ -13,7 +13,6 @@ onready var _preview_ui = $Margin/Preview
 func set_data(data: DialogueData) -> void:
 	_data = data
 	_init_connections()
-	_update_view()
 
 signal scene_added(scene)
 signal scene_removed(scene)
@@ -34,33 +33,36 @@ func _on_scene_action(scene) -> void:
 	_update_view()
 
 func _update_view() -> void:
-	_scene = _data.selected_scene()
-	if _scene:
-		_clear_view()
-		_draw_view()
-	else:
-		_clear_view()
+	_clear_view()
+	if _data:
+		_scene = _data.selected_scene()
+		if _scene:
+			_draw_view()
 
 func _draw_view() -> void:
 	var LoadedScene = load(_scene.resource)
 	_loaded_scene = LoadedScene.instance()
-	
+	_add_reference_rect()
+	var display_size = _data.setting_display_size()
+	var default_size = rect_size
+	var scale = min(default_size.x / display_size.x, default_size.y / display_size.y)
+	_loaded_scene.set_custom_minimum_size(display_size)
+	_loaded_scene.set_scale(Vector2(scale, scale))
+	_preview_ui.add_child(_loaded_scene)
+
+func _add_reference_rect() -> void:
 	var ref_rect = ReferenceRect.new()
 	ref_rect.border_color = Color.white
 	ref_rect.editor_only = false
 	ref_rect.anchor_right  = 1
 	ref_rect.anchor_bottom = 1
 	_loaded_scene.add_child(ref_rect)
-	
-	
-	var _display_size = _data.setting_display_size()
-	var _default_size = rect_size
-	var scale = min(_default_size.x / _display_size.x, _default_size.y / _display_size.y)
-	_loaded_scene.set_custom_minimum_size(_display_size)
-	_loaded_scene.set_scale(Vector2(scale, scale))
-	_preview_ui.add_child(_loaded_scene)
 
 func _clear_view() -> void:
 	for child_ui in _preview_ui.get_children():
 		_preview_ui.remove_child(child_ui)
 		child_ui.queue_free()
+
+
+func _on_DialogueScenePreview_resized():
+	_update_view()
