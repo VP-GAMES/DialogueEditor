36af983d417e56fcd29008c77dba16a1715d3d93
Vladimir Petrenko <vladipetrenko@googlemail.com>
2021-01-03 09:19:27 +0100

    V 0.2.5 Add actor option button for preview dialog

--- addons/dialogue_editor/scenes/scenes/DialogueScenePreviewSentenceDialog.gd
+++ addons/dialogue_editor/scenes/scenes/DialogueScenePreviewSentenceDialog.gd
@@ -1,16 +1,33 @@
+tool
 extends WindowDialog
 
+var _data: DialogueData
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var _actor_ui = $Margin/VBox/HBoxName/Actor
+onready var _texture_ui = $Margin/VBox/Texture
+onready var _type_text_ui = $Margin/VBox/HBoxType/TypeText
+onready var _type_buttons_ui = $Margin/VBox/HBoxType/TypeButtons
+onready var _text_ui = $Margin/VBox/VBoxText/Text
+onready var _add_ui = $Margin/VBox/VBoxButtons/HBoxAdd/Add
+# TODO buttons 
+onready var _clear_ui = $Margin/VBox/HBoxAction/Clear
+onready var _close_ui = $Margin/VBox/HBoxAction/Close
 
+func set_data(data: DialogueData) -> void:
+	_data = data
+	_actor_ui_fill()
+	_init_connections()
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _actor_ui_fill() -> void:
+	_actor_ui.clear()
+	_actor_ui.add_item("None")
+	for actor in _data.actors:
+		if not actor.resources.empty():
+			var image = load(actor.resources[0].path)
+			image = _data.resize_texture(image, Vector2(16, 16))
+			_actor_ui.add_icon_item(image, actor.name)
+		else:
+			_actor_ui.add_item(actor.name)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _init_connections() -> void:
+	pass
