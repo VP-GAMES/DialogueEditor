0ba938e4baa6e047a22d836f2e8d64f62f7b8a15
Vladimir Petrenko <vladipetrenko@googlemail.com>
2021-05-03 18:05:25 +0200

    V 1.3.0 Small bugfix for resources naming

--- addons/dialogue_editor/model/DialogueActor.gd
+++ addons/dialogue_editor/model/DialogueActor.gd
@@ -81,16 +81,17 @@ func change_resource_name(resource: Dictionary, name: String) -> void:
 	var old_name = resource.name
 	if _undo_redo != null:
 		_undo_redo.create_action("Change actor resource name")
-		_undo_redo.add_do_method(self, "_change_resource_name", resource, name)
+		_undo_redo.add_do_method(self, "_change_resource_name", resource, name, true)
 		_undo_redo.add_undo_method(self, "_change_resource_name", resource, old_name)
 		_undo_redo.commit_action()
 	else:
 		_change_resource_name(resource, name)
 
-func _change_resource_name(resource: Dictionary, name: String) -> void:
+func _change_resource_name(resource: Dictionary, name: String, sendSignal = false) -> void:
 	resource.name = name
 	emit_signal("changed")
-	emit_signal("resource_name_changed", resource)
+	if sendSignal:
+		emit_signal("resource_name_changed", resource)
 
 func change_resource_path(resource: Dictionary, path: String) -> void:
 	var old_path = resource.path
